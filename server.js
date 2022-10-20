b17385d644948b0e82ef9b07fc2e9256c485228f
henry <enriqueafrutos@gmail.com>
2022-10-20 17:41:59 -0300

    cambio

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ constructor(archivo){
     this.archivo = archivo;
 }
 async save(object){
+    try{
     const data = await fs.promises.readFile('data/contenedor.json','utf-8');
     const objetos = JSON.parse(data);
     const id = objetos.length + 1;
@@ -12,6 +13,8 @@ async save(object){
     objetos.push(object)
     const objectString = JSON.stringify(objetos)
     await fs.promises.writeFile('data/contenedor.json',objectString)
+    }
+    catch (e){ console.log(e);}
     return objetos;
 }
 
@@ -41,7 +44,7 @@ db.save({titulo:'shampoo', precio:'150'});
 //console.log(await db.getAll());
 const usuario = await db.getById(1);
 console.log(usuario);
-db.deletedById(3)
+//db.deletedById(3)
 //db.deleteAll()
 }
 start()
